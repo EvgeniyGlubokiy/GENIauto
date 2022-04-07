73bdcd822668196151198ca014d2295f484d590e
EvgeniyGlubokiy <eglubokiy@gmail.com>
2022-04-08 02:41:50 +0300

    new

--- js/script.js
+++ js/script.js
@@ -149,3 +149,56 @@ document.querySelector('#more').addEventListener('click', function (e) {
    if (tmp.length < 4)
       this.remove();
 });
+
+//==========================================
+
+//swing================================
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+   menuLinks.forEach(menuLink => {
+      menuLink.addEventListener("click", onMenuLinkClick);
+   });
+
+   function onMenuLinkClick(e) {
+      const menuLink = e.target;
+      if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+         const gotoBlock = document.querySelector(menuLink.dataset.goto);
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header__row').offsetHeight;
+
+         // if (iconMenu.classList.contains('_active')) {
+         //    document.body.classList.remove('_lock');
+         //    iconMenu.classList.remove('_active');
+         //    menuBody.classList.remove('_active');
+         // }
+
+         window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+         });
+         e.preventDefault();
+      }
+   }
+}
+
+// const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+// if (menuLinks.length > 0) {
+//    menuLinks.forEach(menuLink => {
+//       menuLink.addEventListener("click", onMenuLinkClick);
+//    });
+
+//    function onMenuLinkClick(e) {
+//       const menuLink = e.target;
+//       if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+//          const gotoBlock = document.querySelector(menuLink.dataset.goto);
+//          const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYoffset - document.querySelector('.header__row').offsetHeight;
+
+//          window.scrollTo({
+//             top: gotoBlockValue,
+//             behavior: "smooth"
+//          });
+//          e.preventDefault();
+//       }
+//    }
+// }
+
+//=====================================
